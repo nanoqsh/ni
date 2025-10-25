7543cf2d8ddd2994442bfeed8c87a3b6158b8fef
nanoqsh <nanoqsh@gmail.com>
2025-10-26 03:12:43 +0500

    feat: remove `doc_auto_cfg` attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(test), no_std)]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 #[cfg(feature = "std")]
 extern crate std;
